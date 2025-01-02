b81ea79e3934744906e5daf8566bfd46aaeb43e5
EmanueleIaccarino <emanueleiaccarino.ei@gmail.com>
2025-01-02 16:21:42 +0100

    make lib

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='SelfImplemented-Community-Detection-Algorithms-Improved',
     version='0.1.0',
-    packages=find_packages(where='functions'),
-    package_dir={'': 'functions'},
+    packages=find_packages(),
     install_requires=[
         'heapq',
         'collections',
@@ -16,7 +15,7 @@ setup(
     description='A custom library for community detection algorithms with optimized performance and flexibility',
     author='Emanuele Iaccarino',
     author_email='emanueleiaccarino.ei@gmail.com',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(), 
     long_description_content_type='text/markdown',
     url='https://github.com/emanueleiacca/SelfImplemented-Community-Detection-Algorithms-Improved',
     classifiers=[
